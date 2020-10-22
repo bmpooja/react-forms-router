381df09618ff8c2b37cd772dc579d521ac87051d
Puja Bhatamrekar <bmpooja@gmail.com>
2020-10-22 14:27:16 -0700

    added first component

---
+++ src/component/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HomePage() {
+    return(
+        <div className="jumbortron">
+            <h1>Wajooba Administration</h1>
+            <p>React and React Router for ultra-responsive web apps.</p>
+        </div>
+    )
+}
