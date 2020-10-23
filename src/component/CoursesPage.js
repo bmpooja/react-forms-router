cedc02fb19a019ceb802ff910395f41fc7c29d93
Puja Bhatamrekar <bmpooja@gmail.com>
2020-10-23 13:42:38 -0700

    fetching data

--- src/component/CoursesPage.js
+++ src/component/CoursesPage.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import React from "react";
+import { getCourses } from "../api /courseApi";
 
 class CoursesPage extends React.Component {
-    render(){
-        return(
-            <>
-                <h1>Courses</h1>
-            </>
-        )
-    }
+  state = {
+    courses: [],
+  };
+  componentDidMount() {
+    const data = getCourses();
+    console.log("hello  :", data);
+    // getCourses().then((courses) =>
+    //   this.setState({
+    //     courses: courses,
+    //   })
+    // );
+  }
+  render() {
+    return (
+      <>
+        <h1>Courses</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author_id</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.courses.map((item) => {
+              return (
+                <tr>
+                  <td>{item.title}</td>
+                  <td>{item.authorId}</td>
+                  <td>{item.category}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
+    );
+  }
 }
 
 export default CoursesPage;
